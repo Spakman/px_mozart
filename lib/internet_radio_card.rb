5cd22f1c80c683d1c3340af7582062865086256d
Mark Somerville <mark@scottishclimbs.com>
2010-03-09 09:59:11 +0000

    Jog wheel button pauses internet radio now and the display is smarter.

--- lib/internet_radio_card.rb
+++ lib/internet_radio_card.rb
@@ -9,6 +9,11 @@ module Mozart
   class InternetRadioCard < Spandex::Card
     top_left :back
 
+    jog_wheel_button method: -> do
+      Mozart::Player.instance.play_or_pause
+      show
+    end
+
     def play_streams(streams)
       @playlist = Mozart::Playlist.new
       if streams.respond_to? :split
@@ -26,10 +31,27 @@ module Mozart
     end
 
     def show
-      render %{
-        <button position="top_left">Back</button>
-        <text y="30">Internet radio</text>
-      }
+      render_every 1 do
+        if Mozart::Player.instance.playing?
+          %{
+            <button position="top_left">Back</button>
+            <text y="10">Internet radio</text>
+            <text y="20">#{Mozart::Player.instance.artist}</text>
+            <text y="30">#{Mozart::Player.instance.album}</text>
+            <text y="40">#{Mozart::Player.instance.track}</text>
+          }
+        elsif Mozart::Player.instance.paused?
+          %{
+            <button position="top_left">Back</button>
+            <text halign="centre" valign="centre">Paused</text>
+          }
+        else
+          %{
+            <button position="top_left">Back</button>
+            <text halign="centre" valign="centre">Connecting...</text>
+          }
+        end
+      end
     end
   end
 end
